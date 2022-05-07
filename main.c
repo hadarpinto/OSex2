d65801ea4f9ea3caf043b4ea09dcca278103a13f
hadar <hadarpinto@gmail.com>
2022-05-07 19:56:40 +0300

    ex21 ready (limit of 200 chars)

--- main.c
+++ main.c
@@ -21,20 +21,30 @@ void close_files(int fd1, int fd2) {
     close(fd1);
     close(fd2);
 }
+void init_arr(char *arr) {
+    int i;
+    for (i =0 ; i < 200; i++){
+        arr[i] = '\0';
+    }
+}
 
 int main(int argc, char **argv) {
     int fd1, fd2, size;
     char buf1[200], buf2[200];
-    if (argc != 2) {
+    init_arr(buf1);
+    init_arr(buf2);
+
+    if (argc != 3) {
         exit(0);
     }
-    fd1 = open(argv[0], O_RDONLY);
-    fd2 = open(argv[1], O_RDONLY);
+    fd1 = open(argv[1], O_RDONLY);
+    fd2 = open(argv[2], O_RDONLY);
     if (fd1 < 0 || fd2 < 0) {
         exit(0);
     }
     read(fd1,buf1, 200);
     read(fd2, buf2,200);
+    int a = strcmp(buf1,buf2);
     if (strcmp(buf1,buf2) == 0) {
         close_files(fd1,fd2);
         return 1;
@@ -51,12 +61,11 @@ int main(int argc, char **argv) {
         else{
             int i;
             for(i=0;i< strlen(buf1); i++){
-                if (buf1[i] != buf2[i] || buf1[i]+32 != buf2[i] || buf1[i]-32 != buf2[i]){
+                if (buf1[i] != buf2[i] && buf1[i]+32 != buf2[i] && buf1[i]-32 != buf2[i]){
                     return 2;
                 }
             }
             return 3;
-
         }
     }
 }
